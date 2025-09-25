c25ee9cdad860e0bbc323ca2889e47afac6a8ca7
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-25 10:35:57 +0900

    d

--- 3-Lesson/3-lesson.py
+++ 3-Lesson/3-lesson.py
@@ -5,6 +5,13 @@ arr1 = np.array([[[1,2,3], [4,5,6]], [[7,8,9],[10,11,12]]])
 
 print(arr)
 print(arr1)
+print(arr[0,2])
 
+# Dimensions of the array
 print(arr.ndim)
 print(arr1.ndim)
+
+# Get the desired output
+print(arr[0])
+print(arr1[1])
+print(arr[0]+arr[1])
