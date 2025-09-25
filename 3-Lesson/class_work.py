e7e60997295ba68cbf14122f9a76352c765994b8
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-25 12:09:44 +0900

    +

--- 3-Lesson/class_work.py
+++ 3-Lesson/class_work.py
@@ -8,15 +8,23 @@ print(array1, "\n")
 
 # 3-task
 print('--- This is extracting all the odd numbers ---')
-arr = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12])
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+print(arr, "\n")
 extracted_array = arr[1::2]
-print(extracted_array, "\n")
+print(extracted_array, "\n\n")
 
 # 4-task
 print("--- This is replacing all odd numbers with '-1' ---")
 arr1 = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12])
-arr1[1::2] = -1
 print(arr1, "\n")
+arr1[1::2] = -1
+print(arr1, "\n\n")
 
 # 5-task
-print("--- Here is the 3x3 of all 'Trues'")
+print("--- Here is the 3x3 of all 'Trues ---'")
+array_of_all_trues = np.array([[1, 1, 1],
+                               [1, 1, 1],
+                               [1, 1, 1]])
+print(array_of_all_trues, "\n\n")
+change_of_array = array_of_all_trues.astype(bool)
+print(change_of_array, "\n")
