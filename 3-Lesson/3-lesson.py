6efee7d772cd2daec9cfc921e9d86ac36a59d756
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-25 10:24:54 +0900

    +

--- 3-Lesson/3-lesson.py
+++ 3-Lesson/3-lesson.py
@@ -1,5 +1,10 @@
 import numpy as np
 
-arr = np.array([1,2,3,4,5])
+arr = np.array([[1,2,3,4,5], [6,7,8,9,0]])
+arr1 = np.array([[[1,2,3], [4,5,6]], [[7,8,9],[10,11,12]]])
 
 print(arr)
+print(arr1)
+
+print(arr.ndim)
+print(arr1.ndim)
