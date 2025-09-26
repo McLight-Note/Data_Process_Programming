d7ee33610d3673cc0f2915594b86dbd86cf68a39
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-26 10:25:20 +0900

    +

--- 3-Lesson/class_work.py
+++ 3-Lesson/class_work.py
@@ -5,7 +5,6 @@ import numpy as np
 print('--- This is the 1D array ---')
 array1 = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 print(array1, "\n")
-
 # 3-task
 print('--- This is extracting all the odd numbers ---')
 arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
